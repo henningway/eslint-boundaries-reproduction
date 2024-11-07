cdab99cfe20e32bccc3fe94c127b257c9c684996
Henning Schindler <henning.schindler@peerox.de>
2024-11-07 11:03:43 +0100

    remove file filter

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import boundaries from "eslint-plugin-boundaries";
 import ts from "typescript-eslint";
 
 export default ts.config({
-  files: ["src/**/*.{js,ts,vue}"],
   extends: [boundaries, js.configs.recommended, ...ts.configs.recommended],
   languageOptions: {},
   settings: { "boundaries/elements": [] },
