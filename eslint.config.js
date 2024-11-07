ae8c328726ce448f634aefe2bae1abf19ec3d9e3
Henning Schindler <henning.schindler@peerox.de>
2024-11-07 11:05:43 +0100

    remove typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
 import boundaries from "eslint-plugin-boundaries";
-import ts from "typescript-eslint";
 
-export default ts.config({
-  extends: [boundaries, js.configs.recommended, ...ts.configs.recommended],
-  languageOptions: {},
-  settings: { "boundaries/elements": [] },
-  rules: { ...boundaries.configs.recommended.rules },
-});
+export default [
+  js.configs.recommended,
+  boundaries,
+  {
+    rules: { ...boundaries.configs.recommended.rules },
+  },
+];
