12e1cb2019d1bb145f9455ffaafc8cdde8dcabd5
Henning Schindler <henning.schindler@peerox.de>
2024-11-07 11:06:29 +0100

    remove js config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,6 @@
-import js from "@eslint/js";
 import boundaries from "eslint-plugin-boundaries";
 
 export default [
-  js.configs.recommended,
   boundaries,
-  {
-    rules: { ...boundaries.configs.recommended.rules },
-  },
+  { rules: { ...boundaries.configs.recommended.rules } }
 ];
